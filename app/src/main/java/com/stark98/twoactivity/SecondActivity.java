dde4a059e0c1adb40f59a8e3c7934940c0082e60
Manu <sahumanu021@gmail.com>
2020-06-06 17:14:55 +0530

    Creating a movement fron one Layout to another while passing data info at a time

---
+++ app/src/main/java/com/stark98/twoactivity/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.stark98.twoactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void launchSecondActivity(View view) {
+    }
+}
